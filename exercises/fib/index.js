bb83d7e60a4f30491aaea837a7bbb1c5819dc368
Vicky Rodriguez <vrod8763@gmail.com>
2020-09-29 14:58:49 -0400

    add fib recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,11 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if(n === 1 || n === 2){ return 1
+  } else {
+    return fib(n - 1) + fib (n - 2)
+  }
+}
 
 module.exports = fib;
